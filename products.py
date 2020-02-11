de9534c69699d1783c09014f930a3be186236666
Gary Lai <a0983990529@gmail.com>
2020-02-11 16:07:29 +0800

    add header

--- products.py
+++ products.py
@@ -14,3 +14,12 @@ print(products)
 for p in products:
 	print(p)
 	print(p[0], '的價格是', p[1])
+
+#字串可加乘
+#'abc' + '123' = 'abc123'
+#'abc' * 3 = 'abcabcabc'
+
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
